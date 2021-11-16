bf3bcfdeaa16206930cc2c45d6e05cb5202f1343
Naufal Akbar Nugroho <83068205+nuflakbrr@users.noreply.github.com>
2021-11-16 13:16:33 +0700

    Update Dashboard.js

--- static/js/view/Dashboard.js
+++ static/js/view/Dashboard.js
@@ -13,7 +13,7 @@ export default class extends FormatView {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum earum culpa molestias itaque repudiandae hic laboriosam atque cupiditate? Molestiae nemo fugiat illo error corporis, recusandae et ipsum! Animi, soluta harum.
             </p>
             <p>
-                <a href="/posts" data-link>View recent posts</a>.
+                <a href="/vanilla-singlepage/posts" data-link>View recent posts</a>.
             </p>
         `;
   }
